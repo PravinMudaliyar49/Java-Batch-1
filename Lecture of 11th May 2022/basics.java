ca801209031c92739f41465cae68a19b32915835
Pravin <pravincoding49@gmail.com>
2022-05-11 21:59:23 +0530

    Initial commit

---
+++ Lecture of 11th May 2022/basics.java
@@ -0,0 +1,54 @@
+public class basics{
+    public static void main(String[] args) {
+        int x1 = 21;
+        
+        String u = "Dilshad";
+        
+        char l = '@';
+        
+        long z = 3281892;
+        
+        double e = 3.221;
+        
+        boolean d1 = true;
+        boolean d2 = false;
+        
+        System.out.println(u);
+
+
+        //Single-line comment:
+        
+        int x = 21;         // Yeh raj ka age store kar rha hoo
+        
+        System.out.println(x);
+        
+        //Multi-line comment:
+        
+        /*
+        sldvhwhsdoivfh
+        weiowcvhocjw
+        skdnsdcnsd
+        dnwkednwkedn
+        dlcnwdcwdcn
+        wdcnwocdn
+        */
+
+        int raj32_ka_age_mai_store_kar_rha_hoo = 21;
+        
+        System.out.println(raj32_ka_age_mai_store_kar_rha_hoo);        
+        
+        // Camel-case:
+        
+        int rajKaAge = 21;
+        
+        System.out.println(rajKaAge);
+
+        //Question 1:
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+    }
+}
