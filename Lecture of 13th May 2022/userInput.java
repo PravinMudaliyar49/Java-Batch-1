3c1977bfe5637d6097405f1d8ea0ce804c157eda
Pravin <pravincoding49@gmail.com>
2022-05-13 11:41:48 +0530

    User input and Age calculator

---
+++ Lecture of 13th May 2022/userInput.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class userInput {
+    public static void main(String[] args) {
+        //Permission leni hogi:
+
+        Scanner sc = new Scanner(System.in);
+
+        // int age = sc.nextInt();
+        // System.out.println("User's age input is : " + age + ".");
+
+        String name = sc.nextLine();
+        System.out.println("User's name is " + name + ".");
+        
+
+    }
+}
