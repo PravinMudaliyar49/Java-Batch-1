5eb97ebf43245cb5564111ca7bf444ad28bdbd55
Pravin <pravincoding49@gmail.com>
2022-05-28 22:04:34 +0530

    28th May commit

---
+++ Lecture of 28th May/loopingStatements.java
@@ -0,0 +1,20 @@
+public class loopingStatements {
+    public static void main(String[] args) {
+
+        // System.out.println("This is the start");
+
+        // int i = 0;
+
+        // for (i = 0; i <= 5; i++) {
+
+        // }
+
+        // System.out.println(i);
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // System.out.println("This is the end");
+    }
+}
---
+++ Lecture of 28th May/printNumsInAUserGivenRange.java
@@ -0,0 +1,7 @@
+// package Lecture of 28th May;
+
+public class printNumsInAUserGivenRange {
+    public static void main(String[] args) {
+        
+    }
+}
