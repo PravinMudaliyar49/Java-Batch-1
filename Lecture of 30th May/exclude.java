f0e93955e84e0a01d2d0b70c00fd0fc7be836c80
Pravin <pravincoding49@gmail.com>
2022-05-30 22:31:31 +0530

    30th May

---
+++ Lecture of 30th May/exclude.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class exclude {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        for (int i = n1; i <= n2; i++) {
+            if (i != n3) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
---
+++ Lecture of 30th May/excludeRange.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class excludeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        int n4 = sc.nextInt();
+    }
+}
